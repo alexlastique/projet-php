1cc3a35a09988a2199a0c9e0bb84957d6af2adf4
Alexis Godet <alexis.godet05@gmail.com>
2024-01-16 14:50:53 +0100

    corection user

--- src/db.php
+++ src/db.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $dsn = 'mysql:host=localhost;dbname=projetPHP;port=8888';
+    $dsn = 'mysql:host=localhost;dbname=projetPHP;port=8889';
     $db = new PDO($dsn, 'root', 'root');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }
