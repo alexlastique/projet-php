d3d67ad2edf3c64d7366270abf5c5ad845fd5766
[enzoPOHU] <[enzopohu91@gmail.com]>
2024-01-17 11:34:48 +0100

    filtre par catégorie

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -7,6 +7,16 @@ ob_start();?>
     PAGE HOME
     <form action="/actions/home.php" method="post">
         <input type="text" placeholder="search" name="search">
+        <p>
+    <label for="categorie">Sélectionner une catégorie : </label>
+    <select name="categorie" id="categorie">
+        <option name="filtre" value="tout">tout</option>
+        <option name="filtre" value="manga">manga</option>
+        <option name="filtre" value="poster">poster</option>
+        <option name="filtre" value="figurine">figurine</option>
+        <option name="filtre" value="accesoire">accessoire</option>
+    </select>
+</p>
         <button type="submit">search</button>
     </form>
     <?php foreach($_SESSION["produit"] as $produit):?>
