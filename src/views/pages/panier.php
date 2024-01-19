c15038a23077b3f8f4b595d31fb39c23f71c4243
Alexis Godet <alexis.godet05@gmail.com>
2024-01-19 11:11:20 +0100

    modif panier

--- src/views/pages/panier.php
+++ src/views/pages/panier.php
@@ -5,36 +5,10 @@ $title = 'home';
 ob_start();?>
 <?php
 echo "<h1>Mon Panier</h1>";
-$disponibility = true;
-$panier = [
-    'naruto' => 12,
-    'One Piece Tome 106' => 5
-          ];
-function verifierDisponibilite($quantite,$valeur,$disponible) {
-    if ($quantite < $valeur) {
-        echo " - Indisponible</p>";
-        $disponible = false;
-    } else {
-        echo " - Disponible</p>";
-    } return $disponible;
+foreach ($_SESSION["panier"] as $nom){
+    echo($nom.'<br>');
 }
-foreach ($panier as $clef => $valeur){
-
-    $dbsql = $db->prepare("SELECT * FROM product WHERE `name` LIKE :nom");
-    $dbsql->execute([
-        ':nom' => $clef
-    ]);
-    $produits =$dbsql->fetchAll();
-    echo($clef);
-
-    foreach ($produits as $row) {
-        $quantite = $row['quantity'];
-        $disponibility = verifierDisponibilite($quantite, $valeur,$disponibility);
-    }
-}
-if (!$disponibility) {
-    echo 'commande ne peut pas être passer !';
-}
+echo('<a href="/?p=commande"><button>commander</button></a><br>');
 ?>
 
 <?php
